84d8190cd1ce29be6f3873a409095f6e354b431c
Vadim <vadim.kurn@gmail.com>
2020-08-17 07:29:59 +0300

    Code source files added

---
+++ src/test/java/ru/netology/domain/ConditionerTest.java
@@ -0,0 +1,17 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConditionerTest {
+    @Test
+    public void shouldGetAndSet() {
+        Conditioner conditioner = new Conditioner();
+        String expected = "Кондишн";
+
+        assertNull(conditioner.getName());
+        conditioner.setName(expected);
+        assertEquals(expected, conditioner.getName());
+    }
+}
